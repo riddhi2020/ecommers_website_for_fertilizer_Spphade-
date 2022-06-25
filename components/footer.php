5fc9e2474d42669e042658d0176b9a7c1cb5e5fd
riddhi2020 <86001608+riddhi2020@users.noreply.github.com>
2022-06-25 10:51:57 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:+919422647161"><i class="fas fa-phone"></i> +91 9422647161</a>
+         <a href="tel:+919011574575"><i class="fas fa-phone"></i> +91 9011574575</a>
+         <a href="mailto:sunilsphade@yahoo.com"><i class="fas fa-envelope"></i> sunilsphade@yahoo.com</a>
+         <a href="https://g.page/SPPhade?share"><i class="fas fa-map-marker-alt"></i> Mahaveerpath,Akluj-413101 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/sunil.phade.1" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <!-- <a href="#"><i class="fab fa-twitter"></i>twitter</a> -->
+         <a href="https://instagram.com/sunilphade?utm_medium=copy_link" target="_blank"><i class="fab fa-instagram"></i>instagram</a>
+         <!-- <a href="#"><i class="fab fa-linkedin"></i>linkedin</a> -->
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>SPARK  developers</span> | all rights reserved!</div>
+
+</footer>
